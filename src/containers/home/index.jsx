4982eca7351b33b1be6e3d8c3f46e29b982e28ad
Nathanviana <viananathan03@gmail.com>
2024-10-28 22:13:18 -0300

    initial

---
+++ src/containers/home/index.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <ul>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Home	
