a624a6b2f4ce29724d2b13e533eb0a461b184a64
Nathanviana <viananathan03@gmail.com>
2024-11-02 14:34:02 -0300

    resolvendo conflitos

--- src/components/shared/NavBar/NavBar.jsx
+++ src/components/shared/NavBar/NavBar.jsx
@@ -1,25 +1,23 @@
-import { useState } from "react";
 import "./NavBar.css";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
 
-  const [setMenu] = useState("name");
 
   const menuData = [
     {
       path: "/",
       name: "Home",
     },
-    {
-      path: "/produtos",
-      name: "Produtos",
-    },
-    {
-      path: "/sobre",
-      name: "Sobre",
-    }
+    // {
+    //   path: "/produtos",
+    //   name: "Produtos",
+    // },
+    // {
+    //   path: "/sobre",
+    //   name: "Sobre",
+    // }
   ];
 
 
@@ -33,10 +31,10 @@ const NavBar = () => {
           <Nav className="ms-auto">
             {menuData.map((item, index) => (
               <Nav.Link 
-              as={Link} to={item.path} 
+              as={Link} 
+              to={item.path} 
               className="list_item" 
               key={index}
-              onClick={() => setMenu(item.name)}
               >
                 {item.name}	
               </Nav.Link>
