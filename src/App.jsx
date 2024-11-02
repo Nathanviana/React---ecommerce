a6c08516ebe31615362be10a83695d3ae67f18a9
Nathanviana <viananathan03@gmail.com>
2024-11-02 14:21:37 -0300

    arrumando navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-// import React from "react";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Cadastro from "./components/cliente/cadastro";
 import Login from "./components/cliente/login";
 import ProductList from "./components/produtos/ProductList";
+import NavBar from "./components/shared/NavBar/NavBar";
+import Home from "./pages/home/Home";
 
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/login" element={<Login />} />
         <Route path="/lista" element={<ProductList />} />
---
+++ src/components/shared/ExampleCarouselImage.jsx
@@ -0,0 +1,11 @@
+const ExampleCarouselImage = () => {
+  return (
+    <img
+      className="d-block w-100"
+      src="https://via.placeholder.com/800x400"
+      alt="First slide"
+    />
+  );
+}
+
+export default ExampleCarouselImage
