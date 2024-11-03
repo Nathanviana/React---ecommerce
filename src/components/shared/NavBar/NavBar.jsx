2a7530e1ee4ebbac5b27fda159c593df109c08b3
Nathanviana <viananathan03@gmail.com>
2024-11-03 09:42:04 -0300

    adicionando rotas na navBar

--- src/components/shared/NavBar/NavBar.jsx
+++ src/components/shared/NavBar/NavBar.jsx
@@ -3,8 +3,6 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-
-
   const menuData = [
     {
       path: "/",
@@ -20,29 +18,31 @@ const NavBar = () => {
     // }
   ];
 
-
-
   return (
     <Navbar expand="lg" className="navBar">
       <Container>
-        <Navbar.Brand as={Link} to="/" className="brand">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" className="brand">
+          React-Bootstrap
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             {menuData.map((item, index) => (
-              <Nav.Link 
-              as={Link} 
-              to={item.path} 
-              className="list_item" 
-              key={index}
+              <Nav.Link
+                as={Link}
+                to={item.path}
+                className="list_item"
+                key={index}
               >
-                {item.name}	
+                {item.name}
               </Nav.Link>
             ))}
           </Nav>
-            <Nav className="ms-auto">
-              <button className="btn btn-success">Login</button>
-            </Nav>
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/login" className="btn btn-success">
+              Login
+            </Nav.Link>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
