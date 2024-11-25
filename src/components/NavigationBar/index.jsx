97373937cfbe9d147869a6e695948b067b599f9c
Sol <calysol95@gmail.com>
2024-11-25 04:21:12 -0300

    coloquei os ícones do carrinho e login. também adicionei o ícone de cep

--- src/components/NavigationBar/index.jsx
+++ src/components/NavigationBar/index.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom"; // Importando Link do react-router-dom
 import "./styles.css"; // Importando o CSS
-import cart from "../../assets/icons/cart.svg";
 function NavigationBar() {
 	return (
 		<nav className="navBar">
@@ -22,13 +21,32 @@ function NavigationBar() {
 						</Link>
 					</li>
 					<li className="nav-item">
-						<Link to="/login" className="nav-link login-btn">
-							Login
+						<Link className="nav-link local-btn">
+						<i class="bi bi-geo-alt"></i>
+						<div class="cep-container">
+							<div className="texto-local">
+								<span>Digite seu</span>
+								<span>CEP</span>
+							</div>
+						</div>
 						</Link>
 					</li>
 					<li className="nav-item">
-						<Link to="/carrinho">
-							<img width={40} src={cart} alt="" />
+						<Link to="/login" className="nav-link login-btn">
+						<i class="bi bi-person-circle"></i>
+							<div className="texto-login">
+								<span>Faça</span>
+								<span>Login</span>
+							</div>
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/carrinho" className="nav-link carrinho-container2">
+						<i class="bi bi-cart4"></i>
+							<div className="texto-carrinho">
+								<span>Seu</span>
+								<span>Carrinho</span>
+							</div>
 						</Link>
 					</li>
 				</ul>
